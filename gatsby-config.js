90027fc3fa948572389455f334b02fe709632313
spoyntersmith <spoyntersmith@users.noreply.github.com>
2020-03-27 17:23:04 +0000

    update config and remove form

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,15 +2,15 @@ module.exports = {
   siteMetadata: {
     title: "Séan Poynter-Smith - Web Developer",
     author: "Séan Poynter-Smith",
-    description: "Personal site for a Web Developer in Windsor, UK"
+    description: "Personal site for Web Developer in Windsor, UK"
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'séan-poynter-smith',
+        short_name: 'séan',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
