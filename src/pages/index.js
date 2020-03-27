75aa2042695bd3ac11653c47ee8e11d22ac5fd0d
spoyntersmith <spoyntersmith@users.noreply.github.com>
2020-03-27 17:36:38 +0000

    update form-name key

--- src/pages/index.js
+++ src/pages/index.js
@@ -80,7 +80,7 @@ class HomeIndex extends React.Component {
                         <div className="row">
                             <div className="8u 12u$(small)">
                                 <form name="contact" method="post" netlify>
-                                <input type="hidden" name="contact" value="contact"/>
+                                <input type="hidden" name="form-name" value="contact"/>
                                     <div className="row uniform 50%">
                                         <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
                                         <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
