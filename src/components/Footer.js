35a04a9f1a9eb585216938bb3a466df83deb51c6
spoyntersmith <spoyntersmith@users.noreply.github.com>
2020-03-27 12:50:48 +0000

    update social links and remove demo data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,13 @@ class Footer extends React.Component {
             <div id="footer">
                 <div className="inner">
                     <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                        <li><a href="https://twitter.com/spoyntersmith" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+                        <li><a href="https://github.com/spoyntersmith" className="icon fa-github"><span className="label">Github</span></a></li>
+                        {/* <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li> */}
+                        {/* <li><span href="#" className="icon fa-envelope-o"><span className="label">Email</span></span></li> */}
                     </ul>
                     <ul className="copyright">
-                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                        <li>&copy; Séan Poynter-Smith</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
                     </ul>
                 </div>
             </div>
